b898ed188d399bd9714e91945e9bddf5a2bbe7df
rdking <kingmph@gmail.com>
2017-07-21 12:50:55 -0500

    Fixed filePath arguemnt resolution.
    Swapped order of directory trimming and symbolic link resolution.
    Adjusted directory trimming to only trim if the path isn't already a directory.
    Adjusted symbolic link resolution to resolve all the way to the non-link destination.
    Removed disruptive joining of potentially non-existent "../libs" to the resolved path.

--- pathSetup.js
+++ pathSetup.js
@@ -2,19 +2,28 @@ var path = require('path');
 var fs = require('fs');
 
 module.exports = function(filePath) {
-  var orig, p, root, stat;
+  var orig, p, root;
   p = filePath || path.join(__filename, '../../');
-  stat = fs.lstatSync(p);
-  if (stat.isSymbolicLink()) {
+  
+  /**
+   * P needs to be the 1st directory mentioned in filePath. Also, I
+   * swapped this and the symbolic link dereference because if you
+   * dereference before finding the directory, you may end up in a
+   * very different directory than you intended.
+   */
+  p = (fs.lstatSync(p).isDirectory()) ? p : path.dirname(p);
+
+  //Dereference all the way up!
+  while (fs.lstatSync(p).isSymbolicLink()) {
     p = fs.readlinkSync(p);
   }
-  root = path.dirname(p);
-  orig = process.env.NODE_PATH ? process.env.NODE_PATH : '';
-  if (filePath) {
-    process.env.NODE_PATH = path.join(root, '../libs') + path.delimiter + orig;
-  } else {
-    process.env.NODE_PATH = root + path.delimiter + orig;
-  }
+  
+  //Get the original node path, or at least an empty string.
+  orig = process.env.NODE_PATH || '';
+  //Reset the NODE_PATH environment string.
+  process.env.NODE_PATH = root + path.delimiter + orig;
+
+  //Now, just re-initialize node's module paths.
   return require('module')._initPaths();
 };
 
